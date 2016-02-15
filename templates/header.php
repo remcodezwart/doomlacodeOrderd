6519b783f4cadb65ac1429597daf1c8c6be7b7f2
Remco de zwart <remcodezwart1997@gmail.com>
2016-02-15 11:01:45 +0100

    removed
    removed dead code

--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,6 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php echo getTitle($PageContent)?></title>
-	<link rel="stylesheet" href="templates/template.php">
 	<link href="css/<?php echo getTemplate($PageContent)?>.css" rel="stylesheet" type="text/css">
 </head>
 <body>
