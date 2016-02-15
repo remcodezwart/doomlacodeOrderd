5fe85d33cff3a269b4103755473f98e59084c33b
Remco de zwart <remcodezwart1997@gmail.com>
2016-02-15 09:00:30 +0100

    fix
    small fix

--- admin/templates/login.php
+++ admin/templates/login.php
@@ -2,11 +2,11 @@
 	
 		<form method="post" action="#">
 			<label>Gebruiker:</label>
-			<span class="marginUser"></span><input type="text" class="login" placeholder="gebruikersnaam" name="user"></input>
+			<span class="marginUser"></span><input type="text" class="login" placeholder="gebruikersnaam" name="user"><!--input in html has no end tag-->
 			<br>
 			<label>Wachtwoord:</label>
-			<input type="password" class="login" name="password" placeholder="wachtwoord"></input>
+			<input type="password" class="login" name="password" placeholder="wachtwoord"><!--input in html has no end tag-->
 			<br>
-			<input type="submit" class="confirm" value="Aanmelden"></input>
+			<input type="submit" class="confirm" value="Aanmelden"><!--input in html has no end tag-->
 		</form>
 		<p><?php if(isset($erorr)) echo $erorr ?></p>
